0c2c5bae4267e1dadd3cda9995d3312939cd326f
ianmsfvenom <iangamer296@gmail.com>
2025-09-26 20:57:35 -0300

    corrigindo as apis

--- components/box/item-box-list.tsx
+++ components/box/item-box-list.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native"
 import { Text, View } from "react-native"
 import { ThemedView } from "../themed-view"
+import { ThemedText } from "../themed-text"
+import { Ionicons } from "@expo/vector-icons"
 
 interface ItemBoxListProps {
     id: number
@@ -13,16 +15,25 @@ interface ItemBoxListProps {
 
 export default function ItemBoxList(props: ItemBoxListProps) {
     const { id, code, typeBox, status, actual_deposit, actual_location } = props;
+    const theme = useColorScheme();
 
     return (
         <ThemedView style={styles.container} lightColor="#fff" darkColor="#1E1E1E">
+            <View style={styles.iconView}>
+                <Ionicons size={28} name="cube-outline" color={theme === 'dark' ? '#7BF2A8' : '#162C23'} />
+            </View>
             <View style={styles.infoView}>
-                <Text style={styles.infoLabel}>ID: {id}</Text>
-                <Text style={styles.infoLabel}>Codigo: {code}</Text>
-                <Text style={styles.infoLabel}>Tipo: {typeBox}</Text>
-                <Text style={styles.infoLabel}>Status: {status}</Text>
-                <Text style={styles.infoLabel}>Deposito: {actual_deposit}</Text>
-                <Text style={styles.infoLabel}>Local: {actual_location}</Text>
+                <ThemedText style={styles.infoLabel}>ID: {id}</ThemedText>
+                <ThemedText style={styles.infoLabel}>Codigo: {code}</ThemedText>
+                <ThemedText style={styles.infoLabel}>Tipo: {typeBox}</ThemedText>
+                <ThemedText style={styles.infoLabel}>Status: {status}</ThemedText>
+                <ThemedText style={styles.infoLabel}>Deposito: {actual_deposit}</ThemedText>
+                <ThemedText style={styles.infoLabel}>Local: {actual_location}</ThemedText>
+            </View>
+            <View style={styles.detailsView}>
+                <TouchableOpacity style={[styles.detailsButton, theme === 'dark' ? styles.detailsButtonDark : styles.detailsButtonLight]}>
+
+                </TouchableOpacity>
             </View>
         </ThemedView>
     )
@@ -35,8 +46,13 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         borderRadius: 10,
         marginBottom: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
     },
     infoView: {
+        flex: 1,
         flexDirection: "column",
         alignItems: "flex-start",
         justifyContent: "center",
@@ -45,7 +61,27 @@ const styles = StyleSheet.create({
     infoLabel: {
         fontSize: 12,
         lineHeight: 10,
-        marginBottom: 5,
-        color: "red",
+        marginBottom: 5
     },
+    iconView: {
+        flex: 0.3,
+        alignItems: "center",
+        justifyContent: "center",
+        display: 'flex'
+    },
+    detailsView: {
+        flex: 0.3,
+        alignItems: "center",
+        justifyContent: "center",
+        display: 'flex'
+    },
+    detailsButtonLight: {
+        
+    },
+    detailsButtonDark: {
+        
+    },
+    detailsButton: {
+
+    }
 });
