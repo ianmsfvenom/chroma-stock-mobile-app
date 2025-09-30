85374b94534c9b562eca39697d8eede27832f09d
ianmsfvenom <iangamer296@gmail.com>
2025-09-30 19:22:44 -0300

    refatorando autenticacao de login

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,6 @@ export default function Index() {
             })
 
             if (requestCheckToken.ok) return router.replace('/(tabs)/product');
-
             await AsyncStorage.removeItem('access_token');
             Alert.alert('Atenção', 'Sua sessão expirou! Faça login novamente! Status: ' + requestCheckToken.status);
             return router.replace('/login');
